c48cd1059707b0af74473a44a4e220a9db44a59d
Aditya parida <adityaparida917@gmail.com>
2026-05-21 16:35:08 +0530

    url updated

--- server.js
+++ server.js
@@ -15,8 +15,8 @@ const app = express();
 const port=4000;
 
 const allowedOrigins=[
-    "http://localhost:5173",
-    "http://localhost:5174"
+    "https://frontend-hms-alpha.vercel.app/",
+    "https://admin-hms-eight.vercel.app/"
 ];
 
 //Middlewares
